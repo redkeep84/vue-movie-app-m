1b9916dcbb2388f6e1845b0a8e901663f5bc1916
redkeep84 <ckdfud@gmail.com>
2022-12-31 20:02:07 +0900

    비동기 테스트

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,22 +1,8 @@
-const userA = {
-  name: 'HEROPY',
-  age: 85
-}
+import { asyncFn } from "./example";
 
-const userB = {
-  name: 'Neo',
-  age: 22
-}
-
-test('데이터가 일치해야 합니다.', () => {
-  expect(userA.age).toBe(85)
-  expect(userA).toEqual({
-    name: 'HEROPY',
-    age: 85
-  })
-})
-
-test('데이터가 일치하지 않아야 합니다.', () => {
-  expect(userB.name).not.toBe('HEROPY')
-  expect(userB).not.toEqual(userA)
+describe('비동기 테스트', () => {
+  test('async/await', async() => {
+    const res = await asyncFn()
+    expect(res).toBe('Done!')
+  }, 7000)
 })

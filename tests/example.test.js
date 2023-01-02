bf040a28052cbb7c4048a16eed974a886a512bc4
redkeep84 <ckdfud@gmail.com>
2023-01-02 15:35:16 +0900

    모의(Mock)함수

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,8 +1,18 @@
-import { asyncFn } from "./example";
+import axios from 'axios';
+import { fetchMovieTitle } from './example'
 
 describe('비동기 테스트', () => {
-  test('async/await', async() => {
-    const res = await asyncFn()
-    expect(res).toBe('Done!')
-  }, 7000)
+  test('영화 제목 변환', async () => {
+    axios.get = jest.fn(() => {
+      return new Promise(resolve => {
+        resolve({
+          data: {
+            Title: 'Frozen II'
+          }
+        })
+      })
+    })
+    const title = await fetchMovieTitle()
+    expect(title).toBe('Frozen ii')
+  })
 })

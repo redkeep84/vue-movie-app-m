746c5710aa8f84d2a9c9eec585ce8b34783415b9
redkeep84 <ckdfud@gmail.com>
2023-01-02 17:29:45 +0900

    VTU 첫테스트 및 package 재설치

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,18 +1,14 @@
-import axios from 'axios';
-import { fetchMovieTitle } from './example'
+import { mount } from '@vue/test-utils';
+import Example from './Example.vue';
 
-describe('비동기 테스트', () => {
-  test('영화 제목 변환', async () => {
-    axios.get = jest.fn(() => {
-      return new Promise(resolve => {
-        resolve({
-          data: {
-            Title: 'Frozen II'
-          }
-        })
-      })
-    })
-    const title = await fetchMovieTitle()
-    expect(title).toBe('Frozen ii')
+test('메세지를 변경합니다', async () => {
+  const wrapper = mount(Example)
+  // wrapper.vm === this
+  expect(wrapper.vm.msg).toBe('Hello Vue test utils!')
+  // wrapper.vm.msg = 'Hello HEROPY!'
+  await wrapper.setData({
+    msg: 'Hello HEROPY!'
   })
+  expect(wrapper.vm.msg).toBe('Hello HEROPY!')
+  expect(wrapper.find('div').text()).toBe('Hello HEROPY!')
 })
